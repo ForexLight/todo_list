d81420bcebf4317a9a88d00ae7e9d4b1004c91b1
vlad <bbladik12@gmail.com>
2022-01-06 16:37:28 +0200

    created app logic and routers

---
+++ routes/todos.js
@@ -0,0 +1,53 @@
+const { Router } = require('express')
+const Todo = require('../models/Todo')
+const router = Router()
+
+router.get('/', async (req, res) => {
+  const todos = await Todo.find({})
+
+  res.render('index', {
+    title: 'Todos list',
+    isIndex: true,
+    todos
+  })
+})
+
+router.get('/create', (req, res) => {
+  res.render('create', {
+    title: 'Create todo',
+    isCreate: true
+  })
+})
+
+router.post('/create', async (req, res) => {
+  const todo = new Todo({
+    title: req.body.title
+  })
+
+  await todo.save()
+  res.redirect('/')
+})
+
+router.post('/complete', async (req, res) => {
+  const todo = await Todo.findById(req.body.id)
+
+  todo.completed = !!req.body.completed
+  await todo.save()
+
+  res.redirect('/')
+})
+router.get('/delete', async (req,res)=>{
+      const todos = await Todo.find({})
+      res.render('delete', {
+        title: 'delete todo',
+        isIndex: true,
+        todos
+      })
+}
+)
+router.post('/delete',async (req, res) => {
+  const todo = await Todo.findById(req.body.id)
+  todo.remove()
+  res.redirect('/')
+})
+module.exports = router
